97ec1beb21c630bacfc2a4fa5a91991f590477ea
Nipun Sandaruwan <166252579+sandaruwan-98@users.noreply.github.com>
2024-07-22 02:26:39 +0530

    Add files via upload

---
+++ Trees/Activity01.c
@@ -0,0 +1,18 @@
+//create structure to store values in the tree
+//left and right section must be included with tha data part 
+
+
+struct node{
+	int data;
+	struct node *left;
+	struct node *right;
+};
+
+int main(){
+	
+	
+return 0;
+}
+
+
+
